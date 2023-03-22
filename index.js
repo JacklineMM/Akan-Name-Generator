592ac1cf902baac14fdbb6ef4b98630dde1e70c7
Jackline Makena <jacquemackena@gmail.com>
2023-03-22 19:23:49 +0300

    added DOM listener

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () =>{
 let submit = document.getElementById('assign')
 console.log(submit);
+submit.addEventListener("click", () => {
+    
+})
 
 
 })
